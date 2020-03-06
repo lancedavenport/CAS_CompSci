b0aedff99d398bcd8bfd33ef145bc9591db5e0f7
lancedavenport <48220214+lancedavenport@users.noreply.github.com>
2020-03-06 09:11:09 -0800

    Add files via upload

--- imageFilters/imageFilters/ViewController.swift
+++ imageFilters/imageFilters/ViewController.swift
@@ -10,28 +10,38 @@ import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+   
     @IBOutlet weak var imageView: UIImageView!
+    var orginalPhoto: UIImage!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    var filterChoice:String = ""
+    var CIFilterNames = [
+        "CIPhotoEffectChrome",
+        "CIPhotoEffectFade",
+        "CIPhotoEffectInstant",
+        "CIPhotoEffectNoir",
+        "CIPhotoEffectProcess",
+        "CIPhotoEffectTonal",
+        "CIPhotoEffectTransfer",
+        "CISepiaTone"
+    ]
     @IBAction func photoButton(_ sender: Any) {
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera) {
             let imagePicker = UIImagePickerController()
             imagePicker.delegate = self
             imagePicker.sourceType = .camera
-            imagePicker.sourceType = .photoLibrary
+            
             self.present(imagePicker, animated: true, completion: nil)
         }
     }
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        self.dismiss(animated: true, completion: nil)
-        let image = info[UIImagePickerController.InfoKey.originalImage]as! UIImage
-        imageView.image = image
-        let inputImage = image
+    @IBAction func fadeFilter(_ sender: Any) {
         let context = CIContext(options:nil)
-        if let filter = CIFilter(name: "CISepiaTone") {
-            let beginImage = CIImage(image: inputImage)
+        if let filter = CIFilter(name: "CIPhotoEffectFade" ) {
+            let beginImage = CIImage(image: orginalPhoto)
             filter.setDefaults()
             filter.setValue(beginImage, forKey: kCIInputImageKey)
             
@@ -43,6 +53,44 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             }
         }
         
+    }
+    @IBAction func chromeFilter(_ sender: Any) {
+        let context = CIContext(options:nil)
+        if let filter = CIFilter(name: "CIPhotoEffectChrome" ) {
+            let beginImage = CIImage(image: orginalPhoto)
+            filter.setDefaults()
+            filter.setValue(beginImage, forKey: kCIInputImageKey)
+            
+            if let output = filter.outputImage {
+                if let cgimg = context.createCGImage(output, from: output.extent) {
+                    let processedImage = UIImage(cgImage: cgimg)
+                    imageView.image = processedImage
+                }
+            }
+        }
+    }
+    @IBAction func noirFilter(_ sender: Any) {
+        let context = CIContext(options:nil)
+        if let filter = CIFilter(name: "CIPhotoEffectNoir" ) {
+            let beginImage = CIImage(image: orginalPhoto)
+            filter.setDefaults()
+            filter.setValue(beginImage, forKey: kCIInputImageKey)
+            
+            if let output = filter.outputImage {
+                if let cgimg = context.createCGImage(output, from: output.extent) {
+                    let processedImage = UIImage(cgImage: cgimg)
+                    imageView.image = processedImage
+                }
+            }
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        self.dismiss(animated: true, completion: nil)
+        let image = info[UIImagePickerController.InfoKey.originalImage]as! UIImage
+        imageView.image = image
+        orginalPhoto = image
+        imageView.transform = CGAffineTransform(rotationAngle: .pi/2)
     }
     
 }
