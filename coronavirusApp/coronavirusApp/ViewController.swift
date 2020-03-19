e119da54bdc493266aecf7903402f27a50b6672a
lancedavenport <48220214+lancedavenport@users.noreply.github.com>
2020-03-18 23:22:00 -0700

    Add files via upload

---
+++ coronavirusApp/coronavirusApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coronavirusApp
+//
+//  Created by Lance Davenport on 3/18/20.
+//  Copyright © 2020 Lance Davenport. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
